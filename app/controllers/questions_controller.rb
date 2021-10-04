439ead85d713298757a96e60c20170d5329a0bf2
Michael Carter <hickorytandem@gmail.com>
2021-10-04 21:11:29 +0900

    adjusted picture size and put in quetions/answer logic.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,6 +5,16 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+    if @question.downcase.include? "black cat" && "?"
+      @cat_answer = "I don't talk about myself."
+    elsif @question.end_with?"?"
+      @cat_answer = "Should we ever care about such things?"
+    elsif @question.downcase.start_with?"i"
+      @cat_answer = "We shouldn't always think of ourselves, right?"
+    else
+      @cat_answer = "Wonderful! The meaning of life is to take value in your experience. Go forth brave life soldier!"
+    end
 
   end
 
